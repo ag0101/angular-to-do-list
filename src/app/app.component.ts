ab9e99d657875c6cfe37a12443282029b337ea94
ag0101 <ankitagoenka23@gmail.com>
2020-07-04 16:19:43 +0530

    Added new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component, VERSION } from '@angular/core';
 
+
 @Component({
   selector: 'my-app',
-  template: `<h1>
-    Welcome to {{title}}!
-  </h1>
-  <app-input-button-unit></app-input-button-unit>`,
+  templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  title = 'todo-list';
+  title = 'My To Do List APP';
+  
 }
--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  template: `
-    <p>
-      input-button-unit works!
-      The title is: {{ title }}
-    </p>
-  `,
+  templateUrl: "./input-button-unit.component.html",
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
+  @Output() submit : EventEmitter<string> = new EventEmitter();
   title = 'Hello World';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  submitValue(newTitle: string) {
+    this.submit.emit(newTitle);
+  }
 }
